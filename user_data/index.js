6aa3d89ca6ab740e9398262555f1f9f07ff6dbb6
AzamatUzakov <azamatakmal053@gmail.com>
2024-09-27 11:21:37 +0500

    fixed7

--- user_data/index.js
+++ user_data/index.js
@@ -1,18 +1,15 @@
 const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const modal = document.getElementById('modal');
-const overlay = document.getElementById('overlay');
 
 // Открыть модалку
 openModalBtn.addEventListener('click', () => {
     modal.classList.add('open');
-    overlay.classList.add('show');
 });
 
 // Закрыть модалку при клике на крестик
 closeModalBtn.addEventListener('click', () => {
     modal.classList.remove('open');
-    overlay.classList.remove('show');
 });
 
 // Закрыть модалку при клике на затемнение
